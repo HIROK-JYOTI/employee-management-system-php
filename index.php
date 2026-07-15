13dc8252e2ae27f7af076efc89995ac98be26f47
unknown <saikia.hirakjyoti2015@gmail.com>
2026-07-15 23:28:43 +0530

    Create project layout and reusable template files

--- index.php
+++ index.php
@@ -1,6 +1,36 @@
 <?php
-include_once 'config/database.php';
+require_once "config/database.php";
+require_once "includes/header.php";
+?>
 
-echo "Database connection established successfully.";   
+<div class="container mt-5">
 
+    <div class="card shadow">
+
+        <div class="card-body">
+
+            <h2 class="text-center text-primary">
+                Employee Management System
+            </h2>
+
+            <hr>
+
+            <p class="text-center">
+                Welcome Buddy 😊
+            </p>
+
+            <div class="text-center">
+                <a href="auth/login.php" class="btn btn-primary">
+                    Login
+                </a>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+
+<?php
+require_once "includes/footer.php";
 ?>
